3333a05bbbcebe4f4b25ec1e847adb8c9f5a87fe
harsh-csbs-2020-cit <harshikam.csbs2020@citchennai.net>
2023-03-10 04:32:04 +0530

    frontend

---
+++ src/pages/PostPage.js
@@ -0,0 +1,39 @@
+import { formatISO9075 } from "date-fns";
+import React, { useEffect, useState, useContext } from "react";
+import { useParams, Link } from "react-router-dom";
+import { UserContext } from "../UserContext";
+
+const PostPage = () => {
+  const [postInfo, setPostInfo] = useState(null);
+  const { userInfo } = useContext(UserContext);
+  const { id } = useParams();
+  useEffect(() => {
+    fetch(`http://localhost:4000/post/${id}`).then((response) => {
+      response.json().then((postInfo) => {
+        setPostInfo(postInfo);
+      });
+    });
+  }, []);
+
+  if (!postInfo) return "";
+  return (
+    <div className="post-page">
+      <div className="postt">
+        <h1>{postInfo.title}</h1>
+        <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+        <div className="author">by {postInfo.author.username}</div>
+        {userInfo.id === postInfo.author._id && (
+            <div className="edit-row">
+                <Link className="edit-btn" to={`/edit/${postInfo._id}`}>Edit this post</Link>
+            </div>
+        )}
+        <div className="image">
+          <img src={`http://localhost:4000/${postInfo.cover}`} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postInfo.content }} />
+      </div>
+    </div>
+  );
+};
+
+export default PostPage;
