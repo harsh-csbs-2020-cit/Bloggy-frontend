3755e5e972f5d3512ab1021b44eda41983f6925c
Harshika Maheswaran <85213206+harsh-csbs-2020-cit@users.noreply.github.com>
2023-03-10 20:48:16 +0530

    Update CreatePost.js

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -44,7 +44,7 @@ const CreatePost = () => {
     data.set("file", files[0]);
     ev.preventDefault();
 
-    const response = await fetch("http://localhost:4000/post", {
+    const response = await fetch("https://bloggy-backend-29h7.onrender.com/post", {
       method: "POST",
       body: data,
       credentials: 'include',
