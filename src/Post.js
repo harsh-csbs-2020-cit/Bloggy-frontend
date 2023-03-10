a64ad0657c3bef4ecc68e9afba1bda395a5d01c4
Harshika Maheswaran <85213206+harsh-csbs-2020-cit@users.noreply.github.com>
2023-03-10 20:53:03 +0530

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -7,7 +7,7 @@ const Post = ({_id,title,summary,content,cover,createdAt,author}) => {
     <div className='post'>
         <div className='image'>
         <Link to={`/post/${_id}`}>
-          <img src={'http://localhost:4000/'+cover}/>
+          <img src={'https://bloggy-backend-29h7.onrender.com/'+cover}/>
         </Link>
         </div>
         <div className='texts'>
